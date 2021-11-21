2149e7e3f261bf5976475be1c2aa7d8b1ef89b2b
Maryam <mary.behzi@gmail.com>
2021-11-21 16:41:48 +0330

    Inflatng the search layout in its Fragment and displaying as defaultNavHost

--- app/src/main/java/com/example/picnic_android_maryambehzi/search/SearchFragment.kt
+++ app/src/main/java/com/example/picnic_android_maryambehzi/search/SearchFragment.kt
@@ -1,7 +1,21 @@
 package com.example.picnic_android_maryambehzi.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.picnic_android_maryambehzi.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding = FragmentSearchBinding.inflate(inflater)
+
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
 
 }
