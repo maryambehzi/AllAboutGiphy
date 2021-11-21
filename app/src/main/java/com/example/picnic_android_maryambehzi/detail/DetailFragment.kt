5372eff30accda667ec8ef9ed9bfc751eddaf4c8
Maryam <mary.behzi@gmail.com>
2021-11-21 11:29:01 +0330

    Android Navigation added to project

---
+++ app/src/main/java/com/example/picnic_android_maryambehzi/detail/DetailFragment.kt
@@ -0,0 +1,6 @@
+package com.example.picnic_android_maryambehzi.detail
+
+import androidx.fragment.app.Fragment
+
+class DetailFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/picnic_android_maryambehzi/search/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.picnic_android_maryambehzi.search
+
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+
+}
