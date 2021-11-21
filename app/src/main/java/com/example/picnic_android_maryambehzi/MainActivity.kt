826d178dd2ee9c663a271b9dc4ad8661de1c7dd3
Maryam <mary.behzi@gmail.com>
2021-11-21 10:55:19 +0330

    First commit. Initializing the project

---
+++ app/src/main/java/com/example/picnic_android_maryambehzi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.picnic_android_maryambehzi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Our MainActivity is only responsible for setting the content view that contains the
+     * Navigation Host.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
