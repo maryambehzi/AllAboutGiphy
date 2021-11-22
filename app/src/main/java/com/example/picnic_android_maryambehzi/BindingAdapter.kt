c29b47d8844d4c2b76da654c6b9589ffe353baed
Maryam <mary.behzi@gmail.com>
2021-11-22 11:41:10 +0330

    Adding Binding adapters for random GIF views

---
+++ app/src/main/java/com/example/picnic_android_maryambehzi/BindingAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.picnic_android_maryambehzi
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.picnic_android_maryambehzi.network.GifModel
+
+@BindingAdapter("setGifTitle")
+fun TextView.setGifTitle(gifModel: GifModel?) {
+    text = gifModel?.title
+}
+
+@BindingAdapter("setGifUrl")
+fun TextView.setGifUrl(gifModel: GifModel?) {
+    text = gifModel?.url
+}
+
+@BindingAdapter("setGifRating")
+fun TextView.setGifRating(gifModel: GifModel?) {
+    gifModel?.let {
+        visibility = View.VISIBLE
+        text = resources.getString(R.string.age_restriction_text, when(it.rating){
+            "g" -> 1
+            "pg" -> 8
+            "pg-13"-> 13
+            "r"-> 18
+            else ->18
+        })
+    }?: run{
+        visibility = View.GONE
+    }
+}
+
+@BindingAdapter("setGifImage")
+fun ImageView.setGifImage(gifModel: GifModel?){
+    Glide.with(context)
+        .asGif()
+        .load(gifModel?.images?.fixedHeight?.url)
+        .into(this)
+}
