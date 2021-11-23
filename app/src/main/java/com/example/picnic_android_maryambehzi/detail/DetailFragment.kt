704d0d8425e2d1003d69f3cffb1ee6ce375087d7
Maryam <mary.behzi@gmail.com>
2021-11-23 09:03:14 +0330

    Adding back button for detail fragment

--- app/src/main/java/com/example/picnic_android_maryambehzi/detail/DetailFragment.kt
+++ app/src/main/java/com/example/picnic_android_maryambehzi/detail/DetailFragment.kt
@@ -5,19 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.picnic_android_maryambehzi.databinding.FragmentDetailsBinding
 
 class DetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val application = requireNotNull(activity).application
+
         val binding = FragmentDetailsBinding.inflate(inflater)
+
         val gifmodel = DetailFragmentArgs.fromBundle(requireArguments()).gifModel
+
         val viewModelFactory = DetailViewModelFactory(gifmodel, application)
-        binding.viewModel = ViewModelProvider(
+
+        val viewModel = ViewModelProvider(
             this, viewModelFactory).get(DetailViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        viewModel.backIsPresses.observe(viewLifecycleOwner, Observer { isPressed ->
+            if (isPressed){
+                view?.let { view -> Navigation.findNavController(view).popBackStack() }
+            }
+        })
+
+
         binding.lifecycleOwner = this
+
         return binding.root
     }
 }
--- app/src/main/java/com/example/picnic_android_maryambehzi/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/picnic_android_maryambehzi/detail/DetailViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.Navigation
 import com.example.picnic_android_maryambehzi.network.GifModel
 
 class DetailViewModel(gifModel: GifModel, app: Application) : AndroidViewModel(app)   {
@@ -11,7 +12,15 @@ class DetailViewModel(gifModel: GifModel, app: Application) : AndroidViewModel(a
     val selectedGif: LiveData<GifModel>
         get() = _selectedGif
 
+    private val _backIsPresses = MutableLiveData<Boolean>(false)
+    val backIsPresses: LiveData<Boolean>
+        get() = _backIsPresses
+
     init {
         _selectedGif.value = gifModel
+    }
+
+    fun onBackPressed(){
+        _backIsPresses.value = true
     }
 }
